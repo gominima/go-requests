817fb4624a37475e32b62b32751b27a8158ee047
shubhaankar <shubhaankar@hotmail.com>
2020-12-27 10:36:39 +0530

    added docs

--- app.go
+++ app.go
@@ -1,19 +1,31 @@
+//requests is a go package to simplify net/http api requests
+//you can declare a variable Request with headers data/body and url and
+//make requests with it
 package requests
 
 //check
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
-	//"net/url"
+	"net/url"
+	"strings"
 )
 
+//Request Struct is to be declared before making a request
+//Ex: ```
+// req := Request{Url: "https://domain.api/abc",
+//                Data: map[string]interface{}{"My request":"hello world"},
+//                Headers: map[string]string{"Authorization":"Token 123jioaf"}}
+// ```
 type Request struct {
 	Url     string
 	Data    map[string]interface{}
 	Headers map[string]string
 }
 
+//Response struct is to be returned after making request
 type Response struct {
 	Status     string
 	Headers    interface{}
@@ -21,6 +33,15 @@ type Response struct {
 	StatusCode int
 }
 
+//Post method is to be used after declaring req with a Data field
+// ```req := Request{Url: "https://domain.api/abc",
+//                Data: map[string]interface{}{"My request":"hello world"},
+//                Headers: map[string]string{"Authorization":"Token 123jioaf"}}
+// res, err := req.Post()
+// body := res.Body //This is of type map[string]interface{}
+// status := res.Status
+// Headers := res.Headers
+// statuscode := res.StatusCode```
 func (r *Request) Post() (Response, error) {
 	postBody, _ := json.Marshal(r.Data)
 	responseBody := bytes.NewBuffer(postBody)
@@ -52,10 +73,47 @@ func (r *Request) Post() (Response, error) {
 func (r *Request) Get() (Response, error) {
 	req, err := http.NewRequest("GET", r.Url, nil)
 
+	if err != nil {
+		return Response{}, err
+	}
+
 	for key, value := range r.Headers {
 		req.Header.Set(key, value)
 	}
+
 	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return Response{}, err
+	}
+	defer resp.Body.Close()
+	response := Response{}
+	response.Status = resp.Status
+	response.StatusCode = resp.StatusCode
+	response.Headers = resp.Header
+	var body map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&body)
+	response.Body = body
+	return response, nil
+}
+
+func (r *Request) PostFormUrlEncoded() (Response, error) {
+	data := url.Values{}
+	for key, value := range r.Data {
+		data.Set(key, fmt.Sprintf("%v", value))
+	}
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPost, r.Url, strings.NewReader(data.Encode()))
+
+	if err != nil {
+		return Response{}, err
+	}
+	for key, value := range r.Headers {
+		req.Header.Set(key, value)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
 	resp, err := client.Do(req)
 
 	if err != nil {
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/Shubhaankar-sharma/go-requests
 
 go 1.15
+
+require (
+	github.com/google/go-cmp v0.5.4 // indirect
+	golang.org/x/mod v0.4.0 // indirect
+	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
+	golang.org/x/tools v0.0.0-20201224043029-2b0845dc783e // indirect
+	golang.org/x/tools/gopls v0.6.1 // indirect
+	honnef.co/go/tools v0.1.0 // indirect
+	mvdan.cc/gofumpt v0.0.0-20201217210418-dbaa36cfdf8e // indirect
+)
