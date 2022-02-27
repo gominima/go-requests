4e5d41d480be196f7c0959d44daf9102eca43d76
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2022-02-27 17:49:31 +0530

    chore: fix readme

--- _example/fetch.go
+++ _example/fetch.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/gominima/go-requests"
 )
 
 func main() {
-     resp, err := goquests.Get(goquests.Request{
-	     URL: "https://random-data-api.com/api/users/random_user",
-	     Data: make(map[string]interface{}),
-	     Headers: make(map[string]string),
-     })
-     if err != nil {
-	     panic(err)
-     }
-     fmt.Print(resp.Body)
+	resp, err := goquests.Get(goquests.Request{
+		URL:     "https://random-data-api.com/api/users/random_user",
+		Data:    make(map[string]interface{}),
+		Headers: make(map[string]string),
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(resp.Body)
+
 }
